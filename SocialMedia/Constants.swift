cb0afa40691a05f0a0756082751220dd1e98a198
Samir Belkahla <Samir@Samspro.local>
2017-11-02 12:38:45 -0700

    Sign in ui custom class complete

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Samir Belkahla on 02/11/2017.
+//  Copyright © 2017 Samir Belkahla. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
